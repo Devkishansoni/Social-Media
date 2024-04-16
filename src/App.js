154369c0667713c025f7c01c3150623cccd7a417
Kishan-Soni-coder <145443528+Kishan-Soni-coder@users.noreply.github.com>
2024-04-16 11:29:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Socialmedia from './Social Media/Socialmedia';
+
+function App() {
+  return (
+    <>
+      <Socialmedia/>
+    </>
+  );
+}
+
+export default App;
